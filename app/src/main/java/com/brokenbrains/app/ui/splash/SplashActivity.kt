268053f8551abbb966393bb6f0e3f3ffa90c16a9
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-04 15:46:42 +0800

    wip: quick boilerplate

---
+++ app/src/main/java/com/brokenbrains/app/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.brokenbrains.app.ui.splash
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
