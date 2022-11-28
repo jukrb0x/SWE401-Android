ce859905cc053fd797c8246694239d3315360fd3
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-29 01:29:57 +0800

    wip: app SSOT

---
+++ app/src/main/java/com/brokenbrains/fitness/AppState.kt
@@ -0,0 +1,13 @@
+package com.brokenbrains.fitness
+
+import androidx.compose.runtime.Stable
+
+fun rememberAppState()  {
+//    return AppState()
+}
+
+
+@Stable // JB todo: see docs about @Stable
+class AppState {
+     // todo: add app state SSOT here
+}
