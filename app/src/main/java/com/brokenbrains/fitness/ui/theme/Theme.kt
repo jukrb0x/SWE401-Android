9dc191a6e759e09107e0f2cf0bb828c899714de1
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-11-30 20:44:22 +0800

    wip: app theme abstraction

--- app/src/main/java/com/brokenbrains/fitness/ui/theme/Theme.kt
+++ app/src/main/java/com/brokenbrains/fitness/ui/theme/Theme.kt
@@ -3,11 +3,9 @@ package com.brokenbrains.fitness.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.Colors
-import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -25,15 +23,13 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = Teal200
 
-    */
-/* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    *//*
+   //  // Other default colors to override
+   //  background = Color.White,
+   //  surface = Color.White,
+   //  onPrimary = Color.White,
+   //  onSecondary = Color.Black,
+   //  onBackground = Color.Black,
+   //  onSurface = Color.Black,
 
 )
 
@@ -58,13 +54,14 @@ fun FitnessTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Neutral0,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -100,9 +97,192 @@ fun FitnessTheme(
         }
     }
 
+    /*val sysUiController = rememberSystemUiController()
+    SideEffect {
+        sysUiController.setSystemBarsColor(
+            color = colors.uiBackground.copy(alpha = AlphaNearOpaque)
+        )
+    }*/
+
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
+
 }
+
+object FitnessTheme {
+    val colors: FitnessAppColors
+        @Composable
+        get() = FitnessTheme.colors
+}
+
+@Composable
+fun AppColorsProvider( // TODO
+    colors: FitnessAppColors,
+    content: @Composable () -> Unit
+) {
+    val colorPalette = remember {
+        colors.copy()
+    }
+    colorPalette.update(colors)
+    // override color palette to content
+    CompositionLocalProvider(LocalAppColors provides colorPalette, content = content)
+}
+
+private val LocalAppColors = staticCompositionLocalOf<FitnessAppColors> {
+    error("No LocalAppColors provided")
+}
+
+@Stable
+class FitnessAppColors(
+    gradient6_1: List<Color>,
+    gradient6_2: List<Color>,
+    gradient3_1: List<Color>,
+    gradient3_2: List<Color>,
+    gradient2_1: List<Color>,
+    gradient2_2: List<Color>,
+    gradient2_3: List<Color>,
+    brand: Color,
+    brandSecondary: Color,
+    uiBackground: Color,
+    uiBorder: Color,
+    uiFloated: Color,
+    interactivePrimary: List<Color> = gradient2_1,
+    interactiveSecondary: List<Color> = gradient2_2,
+    interactiveMask: List<Color> = gradient6_1,
+    textPrimary: Color = brand,
+    textSecondary: Color,
+    textHelp: Color,
+    textInteractive: Color,
+    textLink: Color,
+    tornado1: List<Color>,
+    iconPrimary: Color = brand,
+    iconSecondary: Color,
+    iconInteractive: Color,
+    iconInteractiveInactive: Color,
+    error: Color,
+    notificationBadge: Color = error,
+    isDark: Boolean
+) {
+    var gradient6_1 by mutableStateOf(gradient6_1)
+        private set
+    var gradient6_2 by mutableStateOf(gradient6_2)
+        private set
+    var gradient3_1 by mutableStateOf(gradient3_1)
+        private set
+    var gradient3_2 by mutableStateOf(gradient3_2)
+        private set
+    var gradient2_1 by mutableStateOf(gradient2_1)
+        private set
+    var gradient2_2 by mutableStateOf(gradient2_2)
+        private set
+    var gradient2_3 by mutableStateOf(gradient2_3)
+        private set
+    var brand by mutableStateOf(brand)
+        private set
+    var brandSecondary by mutableStateOf(brandSecondary)
+        private set
+    var uiBackground by mutableStateOf(uiBackground)
+        private set
+    var uiBorder by mutableStateOf(uiBorder)
+        private set
+    var uiFloated by mutableStateOf(uiFloated)
+        private set
+    var interactivePrimary by mutableStateOf(interactivePrimary)
+        private set
+    var interactiveSecondary by mutableStateOf(interactiveSecondary)
+        private set
+    var interactiveMask by mutableStateOf(interactiveMask)
+        private set
+    var textPrimary by mutableStateOf(textPrimary)
+        private set
+    var textSecondary by mutableStateOf(textSecondary)
+        private set
+    var textHelp by mutableStateOf(textHelp)
+        private set
+    var textInteractive by mutableStateOf(textInteractive)
+        private set
+    var tornado1 by mutableStateOf(tornado1)
+        private set
+    var textLink by mutableStateOf(textLink)
+        private set
+    var iconPrimary by mutableStateOf(iconPrimary)
+        private set
+    var iconSecondary by mutableStateOf(iconSecondary)
+        private set
+    var iconInteractive by mutableStateOf(iconInteractive)
+        private set
+    var iconInteractiveInactive by mutableStateOf(iconInteractiveInactive)
+        private set
+    var error by mutableStateOf(error)
+        private set
+    var notificationBadge by mutableStateOf(notificationBadge)
+        private set
+    var isDark by mutableStateOf(isDark)
+        private set
+
+    fun update(other: FitnessAppColors) {
+        gradient6_1 = other.gradient6_1
+        gradient6_2 = other.gradient6_2
+        gradient3_1 = other.gradient3_1
+        gradient3_2 = other.gradient3_2
+        gradient2_1 = other.gradient2_1
+        gradient2_2 = other.gradient2_2
+        gradient2_3 = other.gradient2_3
+        brand = other.brand
+        brandSecondary = other.brandSecondary
+        uiBackground = other.uiBackground
+        uiBorder = other.uiBorder
+        uiFloated = other.uiFloated
+        interactivePrimary = other.interactivePrimary
+        interactiveSecondary = other.interactiveSecondary
+        interactiveMask = other.interactiveMask
+        textPrimary = other.textPrimary
+        textSecondary = other.textSecondary
+        textHelp = other.textHelp
+        textInteractive = other.textInteractive
+        textLink = other.textLink
+        tornado1 = other.tornado1
+        iconPrimary = other.iconPrimary
+        iconSecondary = other.iconSecondary
+        iconInteractive = other.iconInteractive
+        iconInteractiveInactive = other.iconInteractiveInactive
+        error = other.error
+        notificationBadge = other.notificationBadge
+        isDark = other.isDark
+    }
+
+    fun copy(): FitnessAppColors = FitnessAppColors(
+        gradient6_1 = gradient6_1,
+        gradient6_2 = gradient6_2,
+        gradient3_1 = gradient3_1,
+        gradient3_2 = gradient3_2,
+        gradient2_1 = gradient2_1,
+        gradient2_2 = gradient2_2,
+        gradient2_3 = gradient2_3,
+        brand = brand,
+        brandSecondary = brandSecondary,
+        uiBackground = uiBackground,
+        uiBorder = uiBorder,
+        uiFloated = uiFloated,
+        interactivePrimary = interactivePrimary,
+        interactiveSecondary = interactiveSecondary,
+        interactiveMask = interactiveMask,
+        textPrimary = textPrimary,
+        textSecondary = textSecondary,
+        textHelp = textHelp,
+        textInteractive = textInteractive,
+        textLink = textLink,
+        tornado1 = tornado1,
+        iconPrimary = iconPrimary,
+        iconSecondary = iconSecondary,
+        iconInteractive = iconInteractive,
+        iconInteractiveInactive = iconInteractiveInactive,
+        error = error,
+        notificationBadge = notificationBadge,
+        isDark = isDark,
+    )
+}
+
