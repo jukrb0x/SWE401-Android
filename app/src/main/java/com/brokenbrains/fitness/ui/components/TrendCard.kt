718619d0014f0092ce720b0d32b7c6dd9d371e8e
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-12-01 06:27:18 +0800

    build: add constraint layout for Compose

--- app/src/main/java/com/brokenbrains/fitness/ui/components/TrendCard.kt
+++ app/src/main/java/com/brokenbrains/fitness/ui/components/TrendCard.kt
@@ -1,26 +1,24 @@
 package com.brokenbrains.fitness.ui.components
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrendCard(onClick: () -> Unit = {}) {
-    ElevatedCard(onClick = {onClick()}, modifier = Modifier
-        .fillMaxSize()
-        .padding(8.dp)) {
+    ElevatedCard(
+        onClick = { onClick() }, modifier = Modifier
+            .fillMaxSize()
+    ) {
         Text(text = "TrendCard")
 
     }
-        
+
 }
 
 @Composable
