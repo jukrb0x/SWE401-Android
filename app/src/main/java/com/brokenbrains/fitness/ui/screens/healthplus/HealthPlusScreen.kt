5a60524b8531dcfcff3235e21f53915ea1a14871
Denver <denverleejy@gmail.com>
2022-12-01 16:29:14 +0800

    Initialize HealthPlusScreen

--- app/src/main/java/com/brokenbrains/fitness/ui/screens/healthplus/HealthPlusScreen.kt
+++ app/src/main/java/com/brokenbrains/fitness/ui/screens/healthplus/HealthPlusScreen.kt
@@ -1,16 +1,101 @@
 package com.brokenbrains.fitness.ui.screens.healthplus
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.brokenbrains.fitness.TabRoutes
 import com.brokenbrains.fitness.UserRoutes
 import com.brokenbrains.fitness.ui.components.MainScreenColumn
 import com.brokenbrains.fitness.ui.components.MainScreenHeader
+import com.brokenbrains.fitness.ui.screens.*
+import com.brokenbrains.fitness.ui.screens.tabs
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 
+data class HealthPlusTab(
+    val title: String,
+    val component: @Composable () -> Unit
+)
+
+val healthPlusTab = listOf(
+    HealthPlusTab("Health Info") { HealthInfoPage() },
+    HealthPlusTab("Medical") { MedicalPage() },
+    HealthPlusTab("Community") { CommunityPage() },
+)
+
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun HealthPlusScreen(navigateTo: (route: String) -> Unit) {
-    MainScreenColumn() {
-        MainScreenHeader(title = TabRoutes.HealthPlus.title, onAvatarPressed = { navigateTo(UserRoutes.Profile.route) })
-        Text(text = "OK")
+    MainScreenColumn(horizontalPadding = 0.dp) {
+        Box(modifier = Modifier.padding(horizontal = MainScreenHorizontalPaddingValue)) {
+            MainScreenHeader(
+                title = TabRoutes.HealthPlus.title,
+                onAvatarPressed = { navigateTo(UserRoutes.Profile.route) })
+        }
+        Column(modifier = Modifier.fillMaxWidth()) {
+            val pagerState: PagerState = rememberPagerState(initialPage = 0)
+            val coroutineScope = rememberCoroutineScope()
+            TabRow(
+                modifier = Modifier.fillMaxWidth(),
+                selectedTabIndex = pagerState.currentPage,
+            ) {
+                healthPlusTab.forEachIndexed() { index, tab ->
+                    Tab(
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        },
+                        text = { androidx.compose.material3.Text(tab.title) }
+                    )
+                }
+            }
+            HorizontalPager(count = healthPlusTab.size, state = pagerState) { page ->
+                healthPlusTab[page].component()
+            }
+        }
     }
+}
+
+@Composable
+fun HealthInfoPage() {
+    Column {
+        Text(
+            text = "Health Info test"
+        )
+    }
+}
+
+@Composable
+fun MedicalPage() {
+    Column {
+        Text(
+            text = "Medical test"
+        )
+    }
+}
+
+@Composable
+fun CommunityPage() {
+    Column {
+        Text(
+            text = "Community test"
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun HealthPlusScreenPreview() {
+    HealthPlusScreen(navigateTo = {})
 }
