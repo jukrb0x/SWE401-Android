14ffa9cbd86a5cc2a1777683c153670ca45616dd
Jabriel <15688641+jukrb0x@users.noreply.github.com>
2022-12-25 17:31:30 +0800

    fix: moshi

--- app/src/main/java/com/brokenbrains/fitness/network/TestResponse.kt
+++ app/src/main/java/com/brokenbrains/fitness/network/TestResponse.kt
@@ -1,8 +1,10 @@
 package com.brokenbrains.fitness.network
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.util.*
 
+@JsonClass(generateAdapter = true)
 data class TestResponse(
     @Json(name = "date") val date: Date,
     @Json(name = "message") val message: String
